bc9bd283f4c7bab0c310d4641c5307e66199b761
Ahmed <ahmed.hasan@ucalgary.ca>
2020-11-19 11:37:25 -0700

    Update database.sql

--- config/database.sql
+++ config/database.sql
@@ -1,1 +1,22 @@
+DROP DATABASE IF EXISTS ChildDaycare;
 
+CREATE DATABASE ChildDaycare;
+
+USE ChildDaycare;
+
+CREATE TABLE IF NOT EXISTS DAYCARE (
+     DaycareName VARCHAR(100) NOT NULL
+   , DaycareAddress VARCHAR(100) NOT NULL
+   , TotalNumOfCaretakers INT
+   , CONSTRAINT PK_Daycare PRIMARY KEY (DaycareName, DaycareAddress)
+);
+
+CREATE TABLE IF NOT EXISTS PERSON (
+     SIN INT NOT NULL
+   , Name VARCHAR(30)
+   , Gender VARCHAR(30)
+   , Address VARCHAR(50)
+   , Phone INT
+   , StartDate DATE
+   , PRIMARY KEY (SIN)
+);
