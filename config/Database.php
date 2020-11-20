9448745cfeb1d3da7b7f382092e91680a7536c76
Ahmed <ahmed.hasan@ucalgary.ca>
2020-11-20 01:38:17 -0700

    Fix database name bug

--- config/Database.php
+++ config/Database.php
@@ -1,5 +1,11 @@
 <?php
 
+// This is for debugging and testing code. Shows all errors and warnings
+// So we don't have to go through the logs.
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 class Database {
 
     private $servername = 'localhost';
@@ -10,15 +16,16 @@ class Database {
 
     public function connect() {
 
+        // Navigate to the current directory and open sql script. (C:\AppServ\www\CPSC471-DatabaseProject\config)
         // Get the database name from the sql script.
         // Reads the first line which will contain the database name.
-        $f = fopen('database.sql', 'r');
+        $f = fopen(__DIR__ . "/database.sql", "r");
         $string = trim(fgets($f));
         fclose($f);
 
         $pieces = explode(' ', $string);
-        $database = array_pop($pieces);
-        $database = substr($database, 0, -1); // Takes everything but the last char. Remove ; from the string.
+        $this->database = array_pop($pieces);
+        $this->database = substr($this->database, 0, -1); // Takes everything but the last char. Remove ; from the string.
 
         $this->connection = null;
 
--- config/install.php
+++ config/install.php
@@ -1,21 +1,33 @@
 <?php
 
 // Execute the sql script to set up the database (one time).
+// Use  the  mysqli_query()  function to send the database script to the MySQL connection.
 
-include_once './Database.php';
+$servername = 'localhost';
+$username = 'root';
+$password = 'root';
+$database = "";
+$connection;
 
-try{
+try {
 
-    $database = new Database();
-    $connection = $database->connect();
-    $sql = file_get_contents('database.sql');
-    $connection->exec($sql);
-    echo 'Database and tables created successfully.';
+    // Create connection
+    $connection = new PDO('mysql:host=' . $servername . ';dbname=' . $database, $username, $password);
+    // Set error mode. Get exception when we create queries in case something goes wrong
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'Successfully connected to the database.';
+
+}catch(PDOException $exception) {
+
+    echo 'install.php: Failed to connect to MySQL: ' . $exception->getMessage();
 
 }
-catch(PDOException $exception) {
 
-    echo $exception->getMessage();
-}
+$sql = file_get_contents(__DIR__ . '/database.sql');
+
+$stmt = $connection->prepare($sql);
+
+if ($stmt->execute()) echo 'Database and tables created successfully.';
+else echo 'install.php: Could not create database.';
 
 ?>
