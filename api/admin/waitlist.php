f5f2b103e1472d55302fbcbbe014954e1df4ba2e
Ahmed <ahmed.hasan@ucalgary.ca>
2020-11-26 12:35:48 -0700

    Add error checking in waitlist.php

--- api/admin/waitlist.php
+++ api/admin/waitlist.php
@@ -1,12 +1,20 @@
 <?php
 
 // Required HTTP headers
+header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 include_once '../../config/Database.php'; // Bring in database
+
+if ($_SERVER["REQUEST_METHOD"] != "POST") {
+    // set response code - 405 Method not allowed
+    http_response_code(405);
+    echo 'Request method ' . $_SERVER["REQUEST_METHOD"] . ' not allowed';
+    exit();
+}
 
 // Check if any paramters were passed and return that else return an empty string.
 $childName = isset($_GET['ChildName']) ? $_GET['ChildName'] : '';
@@ -17,17 +25,16 @@ $adminEmpId = isset($_GET['SubmittedById']) ? $_GET['SubmittedById'] : '';
 $database = new Database();
 $db = $database->connect();
 
-// SQL statement to call the stored proc
-// Positional paramaters. Act as placeholders.
+// SQL statement to call the stored proc. Positional paramaters - act as placeholders.
 $sql = 'CALL AddToWaitlist(:childName, :familyName, :adminEmpId)';
 
 // Prepare for execution of stored procedure
 $stmt = $db->prepare($sql);
 
 // Clean up and sanitize data: remove html characters and strip any tags
-$childName = htmlspecialchars(strip_tags($childName)); 
-$familyName = htmlspecialchars(strip_tags($familyName)); 
-$adminEmpId = htmlspecialchars(strip_tags($adminEmpId)); 
+$childName = htmlspecialchars(strip_tags($childName));
+$familyName = htmlspecialchars(strip_tags($familyName));
+$adminEmpId = htmlspecialchars(strip_tags($adminEmpId));
 
 // Bind data
 $stmt->bindParam(':childName', $childName);
@@ -44,7 +51,7 @@ if (empty($childName) || empty($familyName) || empty($adminEmpId)) {
 
     echo 'Unable to add to waitlist. Data is incomplete.';
 
-// Make sure that the input data types (field type, length, etc.) matches model
+    // Make sure that the input data types (field type, length, etc.) matches model
 }else if (strlen($childName) > 30 && strlen($familyName) > 30 && strlen($adminEmpId) > 11) {
 
     // set response code - 400 bad request
@@ -55,22 +62,18 @@ if (empty($childName) || empty($familyName) || empty($adminEmpId)) {
 }else {
 
     // Execute stored procedure
-    if ($stmt->execute()){
+    try {
+        $stmt->execute();
         // set response code - 201 created
         http_response_code(201);
-    
+
         echo "Child added to waitlist";
     }
-
-    // If unable to insert into table, tell the user
-    else {
+    catch(PDOException $exception) {
         // set response code - 503 service unavailable
-        http_response_code(503);
-        echo "Unable to add child to waitlist";
-
-        // Print error if something goes wrong.
-        printf("Error: %s \n",$stmt->error);
+        // Show error if something goes wrong.
+        http_response_code(400);
+        echo "Unable to add child to waitlist. " . $exception->getMessage();
     }
 }
-
 ?>
