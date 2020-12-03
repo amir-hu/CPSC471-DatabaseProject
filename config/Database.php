aa46cd6e110d560b4dc10e0dc067e03712c8aeb2
Ahmed <ahmed.hasan@ucalgary.ca>
2020-12-02 19:57:18 -0700

    Update authenticate() in Database.php

--- config/Database.php
+++ config/Database.php
@@ -2,9 +2,9 @@
 
 // This is for debugging and testing code. Shows all errors and warnings
 // So we don't have to go through the logs.
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 
 class Database {
 
@@ -14,43 +14,41 @@ class Database {
     private $database;
     private $connection;
 
+    // REFRENCE: https://www.php.net/manual/en/features.http-auth.php#73386
+    public function authenticate($clearance) {
 
+        // Accounts that would work with the endpoints. Not all have the same clearance level.
+        $validAccounts = array ("A" => array("Hakuna","low"), "Metroid" => array("Prime","med"), "Erin" => array("Matata","high"));
+        $valid_users = array_keys($validAccounts);
 
+        $user = $_SERVER['PHP_AUTH_USER']; // Username that you pass in.
+        $pass = $_SERVER['PHP_AUTH_PW']; // Password that you pass in.
 
-    //REFRENCE: https://www.php.net/manual/en/features.http-auth.php#73386
-    public function authenticate($clearance){
-        $valid_passwords = array ("A" => array("Hakuna","low"),"Metroid" => array("Prime","med"),"Erin" => array("Matata","high"));
-        $valid_users = array_keys($valid_passwords);
-
-        $user = $_SERVER['PHP_AUTH_USER'];
-        $pass = $_SERVER['PHP_AUTH_PW'];
-        //print_r($valid_passwords[$user]);
-        $validated = (in_array($user, $valid_users)) && ($pass == $valid_passwords[$user][0]);
-   
-        
-        
+        $validated = (in_array($user, $valid_users)) && ($pass == $validAccounts[$user][0]);
 
         if (!$validated) {
             header('WWW-Authenticate: Basic realm="My Realm"');
+            // Set response code - 401 Unauthorized
             http_response_code(401);
-            $message = array ("messsage" => "Not authorized");
+            $message = array ("Messsage" => "Username or password is incorrect.");
             echo json_encode($message);
             die ();
-            }
-        $data = new Database();
-        $data->checkClearance( ($valid_passwords[$user][1]), $clearance);
+        }
+
+        $this->checkClearance( ($validAccounts[$user][1]), $clearance );
     }
-    
 
-    
-    public function checkClearance($give,$required){
+    public function checkClearance($userClearanceLevel, $clearanceLevelRequired){
 
-        if($required=="med" && $give=="low" || $required=="high" && $give!="high"){
-            $message = array ("messsage" => "authorization not high enough");
+        if ( ($clearanceLevelRequired == "med" && $userClearanceLevel == "low") || ($clearanceLevelRequired == "high" && $userClearanceLevel != "high") ) {
+            // Set response code - 401 Unauthorized
+            http_response_code(401);
+            $message = array ("Messsage" => "Authorization not high enough.");
             echo json_encode($message);
             die();
         }
     }
+
     public function connect() {
 
         // Navigate to the current directory and open sql script. (C:\AppServ\www\CPSC471-DatabaseProject\config)
@@ -72,7 +70,7 @@ class Database {
             $this->connection = new PDO('mysql:host=' . $this->servername . ';dbname=' . $this->database, $this->username, $this->password);
             // Set error mode. Get exception when we create queries in case something goes wrong
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
+
             //$message = array('Message' => 'Successfully connected to the database.');
             //echo json_encode($message);
 
